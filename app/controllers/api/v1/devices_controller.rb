4b72b8553e8ad76d49419430b7e2ba96a35a3c84
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-29 18:06:42 -0600

    Heroku fixes.

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::DevicesController < ApplicationController
     device_id = params['id']
     gps = Beacon.all.last
     response = MachineService.post(device_id, gps)
+    binding.pry
     if gps.lat == response[:device][:last_location][:lat]
       render status: 202, json: {"message": "Successfully sent location."}
     else
